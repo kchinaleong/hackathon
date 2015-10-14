060c90e7a6cd7b886221d71273263d769cf025d3
Kate Chin Aleong <kate@mediacore.com>
2015-10-13 17:18:04 -0700

    Working on find form

--- flaskapp/views/find.py
+++ flaskapp/views/find.py
@@ -1,12 +1,11 @@
-from flask import render_template
-
+from flask import render_template, flash, redirect, url_for
 from flaskapp import app
 from flaskapp.lib.errors import AppError
 from flask.ext.wtf import Form
-from wtforms import SubmitField, BooleanField
+from wtforms import SubmitField, BooleanField, SelectMultipleField, widgets, RadioField
+from wtforms.validators import Required
 
-
-@app.route('/find')
+@app.route('/find', methods=['GET', 'POST'])
 def find():
     """ Search page """
     form = CriteriaForm()
@@ -15,19 +14,30 @@ def find():
 	if form.save_to_profile.data
 	    save form criteria to user profile if user is signed in
 	 """
-        flash("DB Stuff, Form Submitted, Search!")
-	return redirect(url_for('home'))
+        flash("db stuff, form submitted, search!")
+        return redirect(url_for('home'))
+    print(dir(form))
+    print(form.errors)
+    flash("form not validated")
+    if form.is_submitted():
+        flash("form is submitted")
     return render_template('find.html', form=form)
 
 class CriteriaForm(Form):
-     s1 = BooleanField('search')
-     s2 = BooleanField('search')
-     s3 = BooleanField('search')
-     s4 = BooleanField('search')
-     s5 = BooleanField('search')
-     s6 = BooleanField('search')
-     s7 = BooleanField('search')
-     s8 = BooleanField('search')
-     s9 = BooleanField('search')
-     save = BooleanField('save_to_profile')
-     submit = SubmitField('Search')
+    age1 = BooleanField('Puppy')
+    age2 = BooleanField('Adult')
+    age3 = BooleanField('Retirement Age')
+    size1 = BooleanField('Small')
+    size2 = BooleanField('Medium')
+    size3 = BooleanField('Large')
+    low = BooleanField('Low')
+    normal = BooleanField('Normal')
+    high = BooleanField('High')
+    health = RadioField('Health', validators=[Required()], choices=[('1','Yes'),(0,'No')])
+    behavior = RadioField('Behavior', validators=[Required()],choices=[('1','Yes'),(0,'No')])
+    children = RadioField('Children', validators=[Required()],choices=[('1','Yes'),(0,'No')])	
+    pets = RadioField('Pets', validators=[Required()],choices=[('1','Yes'),(0,'No')])
+    home = RadioField('Home', validators=[Required()],choices=[('1','Apartment'),('2','House with yard'),('3', 'Acreage')])
+    experience = RadioField('Experience', validators=[Required()],choices=[('1','Little'),('3','Medium'),('3', 'High')])
+    save = BooleanField('save_to_profile')
+    submit = SubmitField('Find Adoptables!')
